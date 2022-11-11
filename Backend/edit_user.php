cb19737c2e2a8203412713407837412bcfc507f1
MichelAD1 <michel.a.abidaoud@gmail.com>
2022-11-11 21:05:50 +0200

    Continuing edit_user

--- Backend/edit_user.php
+++ Backend/edit_user.php
@@ -24,7 +24,34 @@ if($conn->connect_error){
             $stmt->execute();
         }
     }
-    
+    if($_POST['full_name']){
+        $full_name=$_POST['full_name'];
+        $stmt = $conn->prepare("UPDATE users SET full_name=? WHERE id=?");
+        $stmt->bind_param("si", $full_name, $id);
+        $stmt->execute();
+    }
+    if($_POST['password']){
+        $password=$_POST['password'];
+        $password=hash('sha256',$password);
+        $stmt = $conn->prepare("UPDATE users SET password=? WHERE id=?");
+        $stmt->bind_param("si", $password, $id);
+        $stmt->execute();
+    }
+    if($_POST['bio']){
+        $bio=$_POST['bio'];
+        $stmt = $conn->prepare("UPDATE users SET bio=? WHERE id=?");
+        $stmt->bind_param("si", $bio, $id);
+        $stmt->execute();
+    }
+    if($_FILES['profile_picture']['name']){
+        $target="profile-pic/".basename($_FILES['profile_picture']['name']);
+        $profile_picture=$_FILES['profile_picture']['name'];
+        $stmt = $conn->prepare("UPDATE users SET profile_picture=? WHERE id=?");
+        $stmt->bind_param("si", $profile_picture, $id);
+        $stmt->execute();
+    }
+    $response["User updated"]=true;
+    echo json_encode($response);
 }
 $stmt->close();
 $conn->close();
