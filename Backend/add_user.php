9c9fbed1854b89a5035208134b07abd8ffd9e7d1
MichelAD1 <michel.a.abidaoud@gmail.com>
2022-11-11 18:01:39 +0200

    Creating add_user and initializing post variables

---
+++ Backend/add_user.php
@@ -0,0 +1,10 @@
+<?php
+include("connection.php");
+
+$username=$_POST['username'];
+$fullname=$_POST['fullname'];
+$password=$_POST['password'];
+$bio=$_POST['bio'];
+$profile_picture=$_POST['profile_picture'];
+
+?>
